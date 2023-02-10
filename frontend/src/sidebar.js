c6aa5f57e308092c48c0308de040e211fa078999
jonathonkindle <jonathonkindle@gmail.com>
2023-02-09 17:50:30 -0800

    Created sidebar for move list and moves to win
    I have created the containers for our future move lists and moves to victory. It is pretty basic at the moment as there is no data yet, but that will change in future sprints

---
+++ frontend/src/sidebar.js
@@ -0,0 +1,10 @@
+const Sidebar = () => {
+    return (
+        <div className="row">
+            <div className="column">Move List</div>
+            <div className="column">Moves to Win</div>
+        </div>
+    );
+}
+
+export default Sidebar;
